fd1da8888f171fc17fc73f399772f1646725d783
Henrique Serres <henriqueserres@gmail.com>
2023-08-08 11:12:31 -0300

    get better at header component

--- src/components/Head/index.tsx
+++ src/components/Head/index.tsx
@@ -3,19 +3,37 @@ import { SiGithub, SiLinkedin } from 'react-icons/si'
 
 export default function Header() {
   return (
-    <header className="flex flex-row  justify-between gap-4 bg-slate-600 p-6 font-mono">
-      <div>
-        <Link href="https://www.linkedin.com/in/henrique-serres/">
+    <header className="flex flex-row justify-between bg-slate-600 p-6 font-mono">
+      <nav className="flex flex-row justify-start gap-5 pl-4">
+        <Link
+          href="https://www.linkedin.com/in/henrique-serres/"
+          className="text-2xl hover:text-slate-300"
+        >
           <SiLinkedin />
         </Link>
-        <Link href="https://github.com/HenriqueSerres">
+        <Link
+          href="https://github.com/HenriqueSerres"
+          className="text-2xl hover:text-slate-300"
+        >
           <SiGithub />
         </Link>
+      </nav>
+      <div>
+        <h1 className="text-4xl">{`Hi, I'm Henrique Serres`}</h1>
       </div>
-      <nav>
-        <Link href="/about">About</Link>
-        <Link href="/projects">Projects</Link>
-        <Link href="/contact">Contact</Link>
+      <nav className="flex flex-row justify-end gap-5 pr-4">
+        <Link href="/" className="text-lg hover:text-slate-300">
+          Home
+        </Link>
+        <Link href="/about" className="text-lg hover:text-slate-300">
+          About
+        </Link>
+        <Link href="/projects" className="text-lg hover:text-slate-300">
+          Projects
+        </Link>
+        <Link href="/contact" className="text-lg hover:text-slate-300">
+          Contact
+        </Link>
       </nav>
     </header>
   )
